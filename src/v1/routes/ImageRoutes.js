82982a3638f3dc725c2ddfa27b1647ec84f69032
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2024-02-20 16:21:20 +0800

    fix delete permission

--- src/v1/routes/ImageRoutes.js
+++ src/v1/routes/ImageRoutes.js
@@ -47,7 +47,7 @@ router.post(
 router.delete(
 	`${prefix}/:id`,
 	auth.protect,
-	auth.restrictedTo('admin'),
+	auth.restrictedTo('super_admin'),
 	catchUnknownError(MainController.delete.bind(MainController)),
 );
 
